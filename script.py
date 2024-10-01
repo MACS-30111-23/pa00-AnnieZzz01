a5f35d8620fd56607085fd65994e104e6bd3c849
AnnieZzz01 <zhiyue2@uchicago.edu>
2024-09-30 19:30:27 -0500

    Update script.py

--- script.py
+++ script.py
@@ -1,3 +1,10 @@
 # This file is intentionally empty
 #
 # add your code for printing out a message, such as 'Hello World!' or something else!
+# This script prints a greeting message to the console
+
+def main():
+    print("Psychology is good!")
+
+if __name__ == "__main__":
+    main()
